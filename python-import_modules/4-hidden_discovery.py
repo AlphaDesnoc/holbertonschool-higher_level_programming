75c31ac5d290184bca68cc283a1c70869ba55e54
root <root@vmi2390732.contaboserver.net>
2025-01-13 18:33:23 +0100

    fix

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
-import marshal
-import types
+import hidden_4
 
 if __name__ == "__main__":
-    pyc_file_path = '/tmp/hidden_4.pyc'
-    with open(pyc_file_path, 'rb') as pyc_file:
-        pyc_file.seek(16)
-        code = marshal.load(pyc_file)
-    module = types.ModuleType("hidden_4")
-    exec(code, module.__dict__)
-    names = [name for name in dir(module) if not name.startswith("__")]
-    names.sort()
-    for name in names:
-        print(name)
+    names = dir(hidden_4)
+    for name in sorted(names):
+        if not name.startswith('__'):
+            print(name)
