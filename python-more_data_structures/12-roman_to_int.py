996bf1e460afcbc209e56c6c728a5ba4bc779854
root <root@vmi2390732.contaboserver.net>
2025-01-20 16:44:00 +0100

    fix

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str):
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
 
     rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
@@ -11,7 +10,12 @@ def roman_to_int(roman_string):
 
     for char in reversed(roman_string):
         current_value = rom.get(char, 0)
-        total += current_value if current_value >= prev_value else -current_value
+
+        if current_value >= prev_value:
+            total += current_value
+        else:
+            total -= current_value
+
         prev_value = current_value
 
     return total
