0b50f19b003467fa0dbfbdac03ce29ee0282e75a
Mickael Chauvin <122469456+AlphaDesnoc@users.noreply.github.com>
2025-02-24 22:42:48 +0100

    fix(python-inheritance): fix 7-base_geometry.py isinstance(value, int) for better compatibility with integer subclasses.

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-"""Defines an empty class BaseGeometry."""
-
+"""Defines a base class for geometry operations."""
 
 class BaseGeometry:
-    """Represent base geometry."""
+    """Base class for geometry objects with validation methods."""
 
     def area(self):
-        """Exception"""
+        """Raises an Exception to indicate that the method is not implemented."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validate a parameter as an integer.
+        """Validates that a value is a positive integer.
 
         Args:
             name (str): The name of the parameter.
             value (int): The parameter to validate.
+
         Raises:
             TypeError: If value is not an integer.
-            ValueError: If value is <= 0.
+            ValueError: If value is less than or equal to 0.
         """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError(f"{name} must be greater than 0")
