e930849eae4c01722e48cdac6a6ce482b665c4af
Mickael Chauvin <122469456+AlphaDesnoc@users.noreply.github.com>
2025-03-03 10:39:01 +0100

    fix(SQL_introduction): Fix the task 5

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,10 +1,2 @@
--- Script that prints the full description of the table first_table
--- from the database hbtn_0c_0 in your MySQL server
--- The database name will be passed as an argument of the mysql command
-
-SELECT CONCAT(
-    'Table   ', TABLE_NAME, '\n',
-    'Create Table: CREATE TABLE `', TABLE_NAME, '` (\n  `id` int DEFAULT NULL,\n  `name` varchar(256) DEFAULT NULL\n) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci'
-) AS '' FROM INFORMATION_SCHEMA.TABLES 
-WHERE TABLE_SCHEMA = DATABASE() 
-AND TABLE_NAME = 'first_table';
+-- Prints the full description of the table first_table in my MySQL server.
+SHOW CREATE TABLE `first_table`;
