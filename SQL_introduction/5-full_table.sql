5ae75a84b7b0f6a09080330d160cc83a008a77a6
Mickael Chauvin <122469456+AlphaDesnoc@users.noreply.github.com>
2025-03-03 10:35:07 +0100

    fix(SQL_introduction): Fix the task 5

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,10 @@
- -- prints the following description of the table first_table from the database hbtn_0c_0   
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+-- Script that prints the full description of the table first_table
+-- from the database hbtn_0c_0 in your MySQL server
+-- The database name will be passed as an argument of the mysql command
+
+SELECT CONCAT(
+    'Table   ', TABLE_NAME, '\n',
+    'Create Table: CREATE TABLE `', TABLE_NAME, '` (\n  `id` int DEFAULT NULL,\n  `name` varchar(256) DEFAULT NULL\n) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci'
+) AS '' FROM INFORMATION_SCHEMA.TABLES 
+WHERE TABLE_SCHEMA = DATABASE() 
+AND TABLE_NAME = 'first_table';
