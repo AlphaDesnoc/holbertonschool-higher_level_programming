f1e2199a2fcd454be8d41daa3fee90bd239eca44
AlphaDesnoc <alphadesnoc@gmail.com>
2025-03-05 14:06:23 +0100

    feat(python-object-relational_mapping): Adding all files from tasks

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,43 +1,22 @@
 #!/usr/bin/python3
-
-""" Lists all states from the database hbtn_0e_0_usa """
-
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    """ Lists all states from the database hbtn_0e_0_usa
-
-        Usage: ./0-select_states.py <mysql username>
-        <mysql password> <database name>
-
-        Arguments:
-            mysql username: username to connect the mySQL database
-            mysql password: password to connect the mySQL database
-            database name: name of the database to connect
-        """
+    # Get MySQL credentials from command-line arguments
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    
     # Connect to the MySQL database
-    db = MySQLdb.connect(
-        host="localhost",
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        port=3306
-    )
-
-    # Create a cursor object to interact with the database
+    db = MySQLdb.connect(host="localhost", user=username, passwd=password, db=database, port=3306)
     cursor = db.cursor()
-
-    # Execute the SQL query to select all states ordered by id
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-    # Fetch all the rows from the executed query
-    rows = cursor.fetchall()
-
-    # Print each row
-    for row in rows:
+    
+    # Execute the SQL query to get all states sorted by id
+    cursor.execute("SELECT * FROM states ORDER BY id ASC;")
+    
+    # Fetch all results and print them
+    for row in cursor.fetchall():
         print(row)
-
-    # Close the cursor and the database connection
+    
+    # Close cursor and database connection
     cursor.close()
     db.close()
