5f6dfa20e70e3bc392972071d7dec368cefeb5e7
AlphaDesnoc <alphadesnoc@gmail.com>
2025-03-05 14:09:52 +0100

    feat(python-object-relational_mapping): Adding all files from tasks

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,6 @@ Lists all states from the database hbtn_0e_0_usa.
 import MySQLdb
 import sys
 
-
 def list_states():
     """
     Connects to a MySQL database and retrieves all states
@@ -17,14 +16,13 @@ def list_states():
     db = MySQLdb.connect(host="localhost", user=username,
                          passwd=password, db=database, port=3306)
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC;")
+    cursor.execute("SELECT id, name FROM states ORDER BY id ASC;")
 
     for row in cursor.fetchall():
         print(row)
 
     cursor.close()
     db.close()
-
 
 if __name__ == "__main__":
     list_states()
